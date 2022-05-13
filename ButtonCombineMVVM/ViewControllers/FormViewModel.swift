b5eed267220dd09a42bf526bc12063aa78931e6f
komplektan <54449303+komplektan@users.noreply.github.com>
2022-05-14 00:32:15 +0500

    Ending project

---
+++ ButtonCombineMVVM/ViewControllers/FormViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FormViewModel.swift
+//  ButtonCombineMVVM
+//
+//  Created by Вячеслав Квашнин on 13.05.2022.
+//
+
+import Foundation
+
+class FormViewModel {
+    @Published var isSubmitAllowed: Bool = false
+}
--- ButtonCombineMVVM/ViewControllers/ViewController.swift
+++ ButtonCombineMVVM/ViewControllers/ViewController.swift
@@ -6,16 +6,26 @@
 //
 
 import UIKit
+import Combine
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     @IBOutlet weak var switchSubscriber: UISwitch!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    private var cancellabel: AnyCancellable?
+    private var viewModel = FormViewModel()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        cancellabel = viewModel.$isSubmitAllowed
+            .receive(on: DispatchQueue.main)
+            .assign(to: \.isEnabled, on: submitButton)
     }
-
-
+    
+    @IBAction func didSwitch(_ sender: Any) {
+        viewModel.isSubmitAllowed = (sender as AnyObject).isOn
+    }
 }
 
